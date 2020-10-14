7618c652300f9b2dc6a633c3f7b983fb899ecec6
mittalrohit0598 <mittalrohit0598@gmail.com>
2020-10-14 21:43:23 +0530

    Add knight_moves method to find shortest path between two squares travelled by a knight

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -31,10 +31,38 @@ class Board
   end
 
   def set_square(file, rank)
-    board[(file - 8).abs][rank - 1].piece = KNIGHT
+    board[(file - 7).abs][rank].piece = KNIGHT
+  end
+end
+
+def backtrace(parent, start, finish)
+  path = [finish]
+  path.unshift(parent[path[0]]) while path[0] != start
+  path
+end
+
+def possible_knight_moves(square)
+  moves = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+  moves.map! { |move| [move[0] + square[0], move[1] + square[1]] }
+  moves.select { |move| (0...8).include?(move[0]) && (0...8).include?(move[1]) }
+end
+
+def knight_moves(start, finish)
+  parent = {}
+  queue = [start]
+  until queue.empty?
+    node = queue.shift
+    return backtrace(parent, start, finish) if node == finish
+
+    possible_knight_moves(node).each do |move|
+      parent[move] = node unless parent.key?(move)
+      queue.push(move)
+    end
   end
 end
 
 board = Board.new
-board.set_square(rand(1..8), rand(1..8))
+path = knight_moves([3, 3], [4, 3])
+path.each { |move| board.set_square(move[0], move[1]) }
 board.print_board
+p path
